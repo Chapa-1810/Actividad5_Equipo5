06ce7f7399d02f83def9089122731b4c067c67b2
Alexis Chapa <93449818+Chapa-1810@users.noreply.github.com>
2022-03-25 17:14:53 -0600

    Update Actividad5.py

--- Actividad5.py
+++ Actividad5.py
@@ -7,7 +7,7 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-marcador=0;
+marcador=0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
